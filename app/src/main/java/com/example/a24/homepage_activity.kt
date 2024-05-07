09c0225227b56418b035e6657ee0a38a90cf547c
JustinTunggal <justintunggal1@gmail.com>
2024-05-07 22:49:11 +0800

    update

--- app/src/main/java/com/example/a24/homepage_activity.kt
+++ app/src/main/java/com/example/a24/homepage_activity.kt
@@ -1,12 +1,37 @@
 package com.example.a24
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class homepage_activity : AppCompatActivity() {
+
+    private lateinit var shopBtn: Button
+    private lateinit var barracksBtn: Button
+    private lateinit var playBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.homepage)
+
+        shopBtn = findViewById(R.id.home_btnshop)
+        barracksBtn = findViewById(R.id.home_btnbarracks)
+        playBtn = findViewById(R.id.playbutton)
+
+        shopBtn.setOnClickListener{
+            val intent = Intent(this, shop_activity::class.java)
+            startActivity(intent)
+        }
+
+        barracksBtn.setOnClickListener{
+            val intent = Intent(this, barracks_activity::class.java)
+            startActivity(intent)
+        }
+
+        playBtn.setOnClickListener{
+            val intent = Intent(this, play_activity::class.java)
+            startActivity(intent)
+        }
     }
 }
